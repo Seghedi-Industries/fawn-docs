80dee612014898d209961681b5be1ac3371f5e41
Seghedi Robert <robertseghedi123@gmail.com>
2021-08-25 00:02:03 +0300

    docs init

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,261 +1,108 @@
+const lightCodeTheme = require('prism-react-renderer/themes/github');
+const darkCodeTheme = require('prism-react-renderer/themes/dracula');
+
+/** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: "Fawn",
-  tagline: "Documentation and Guides",
-  url: "https://docs.uniswap.org/",
-  baseUrl: "/",
-  onBrokenLinks: "warn",
-  onBrokenMarkdownLinks: "ignore",
-  favicon: "img/favicon.png",
-  organizationName: "Seghedi Industries", // Usually your GitHub org/user name.
-  projectName: "faws-docs", // Usually your repo name.
+  title: 'My Site',
+  tagline: 'Dinosaurs are cool',
+  url: 'https://your-docusaurus-test-site.com',
+  baseUrl: '/',
+  onBrokenLinks: 'throw',
+  onBrokenMarkdownLinks: 'warn',
+  favicon: 'img/favicon.ico',
+  organizationName: 'facebook', // Usually your GitHub org/user name.
+  projectName: 'docusaurus', // Usually your repo name.
   themeConfig: {
-    image: "img/twitter_card_bg.jpg",
-    prism: {
-      additionalLanguages: ["solidity"],
-    },
-    algolia: {
-      apiKey: "32465e2ab6f7554ff014e64c0d92171c",
-      indexName: "v3-docs",
-      appId: "S0IDD0YGLZ",
-    },
     navbar: {
-      title: "Fawn Docs",
+      title: 'My Site',
       logo: {
-        alt: "Fawn Logo",
-        src: "img/fawn_logo.svg",
+        alt: 'My Site Logo',
+        src: 'img/logo.svg',
       },
       items: [
         {
-          type: "docsVersionDropdown",
-          //// Optional
-          position: "left",
-          dropdownActiveClassDisabled: true,
-          docsPluginId: "default",
-          className: "persistent",
+          type: 'doc',
+          docId: 'intro',
+          position: 'left',
+          label: 'Tutorial',
         },
+        {to: '/blog', label: 'Blog', position: 'left'},
         {
-          to: "/protocol/reference/smart-contracts",
-          label: "Contracts",
-          position: "left",
-          className: "V3_active",
-        },
-        {
-          to: "/sdk/introduction",
-          label: "SDK",
-          position: "left",
-          className: "V3_active",
-        },
-        {
-          to: "/sdk/Subgraph%20data",
-          label: "Subgraph (API)",
-          position: "left",
-          className: "V3_active",
-        },
-        {
-          to: "/protocol/concepts/governance/governance-reference",
-          label: "Governance",
-          position: "left",
-          className: "V3_active",
-        },
-        {
-          to: "/protocol/V2/guides/smart-contract-integration/quick-start",
-          label: "Contracts",
-          position: "left",
-          className: "V2_active",
-        },
-        {
-          to: "/sdk/2.0.0/introduction",
-          label: "SDK",
-          position: "left",
-          className: "V2_active",
-        },
-        {
-          to: "/protocol/V2/reference/API/overview",
-          label: "Subgraph (API)",
-          position: "left",
-          className: "V2_active",
-        },
-        {
-          to: "/protocol/V2/reference/Governance/governance-reference",
-          label: "Governance",
-          position: "left",
-          className: "V2_active",
-        },
-        {
-          to: "/protocol/V1/guides/connect-to-uniswap",
-          label: "Contracts",
-          position: "left",
-          className: "V1_active",
-        },
-        {
-          to: "/sdk/1.0.0/introduction",
-          label: "SDK",
-          position: "left",
-          className: "V1_active",
-        },
-        {
-          href: "https://github.com/Seghedi-Industries/fawn",
-          label: "GitHub",
-          position: "right",
-          className: "persistent",
-        },
-        {
-          type: "localeDropdown",
-
-          //// Optional
-          position: "right",
-          // Add additional dropdown items at the beginning/end of the dropdown.
-          dropdownItemsBefore: [],
-          dropdownItemsAfter: [
-            {
-              to: "https://my-site.com/help-us-translate",
-              label: "Help us translate",
-            },
-          ],
+          href: 'https://github.com/facebook/docusaurus',
+          label: 'GitHub',
+          position: 'right',
         },
       ],
     },
     footer: {
-      // style: "dark",
+      style: 'dark',
       links: [
         {
-          title: "Developers",
+          title: 'Docs',
           items: [
             {
-              label: "Bug Bounty",
-              href: "https://github.com/Uniswap/uniswap-v3-periphery/blob/main/bug-bounty.md",
-            },
-            {
-              label: "#dev-chat",
-              href: "https://discord.gg/ybKVQUWb4s",
-            },
-            {
-              label: "Whitepaper",
-              href: "https://uniswap.org/whitepaper-v3.pdf",
+              label: 'Tutorial',
+              to: '/docs/intro',
             },
           ],
         },
         {
-          title: "Github",
+          title: 'Community',
           items: [
             {
-              label: "uniswap-v3-core",
-              href: "https://github.com/Uniswap/uniswap-v3-core",
+              label: 'Stack Overflow',
+              href: 'https://stackoverflow.com/questions/tagged/docusaurus',
             },
             {
-              label: "uniswap-v3-sdk",
-              href: "https://github.com/Uniswap/uniswap-v3-sdk",
+              label: 'Discord',
+              href: 'https://discordapp.com/invite/docusaurus',
             },
             {
-              label: "uniswap-v3-periphery",
-              href: "https://github.com/Uniswap/uniswap-v3-periphery",
-            },
-            {
-              label: "Deployment addresses",
-              href: "https://github.com/Uniswap/uniswap-v3-periphery/blob/main/deploys.md",
+              label: 'Twitter',
+              href: 'https://twitter.com/docusaurus',
             },
           ],
         },
         {
-          title: "Ecosystem",
+          title: 'More',
           items: [
             {
-              label: "Home",
-              href: "https://uniswap.org/",
+              label: 'Blog',
+              to: '/blog',
             },
             {
-              label: "App",
-              href: "https://app.uniswap.org/",
-            },
-            {
-              label: "Analytics",
-              href: "https://info.uniswap.org/home",
-            },
-            {
-              label: "Token Lists",
-              href: "https://tokenlists.org/",
-            },
-            {
-              label: "Brand Assets",
-              href: "https://uniswap.org/Uniswap_brand_assets.zip",
-            },
-          ],
-        },
-        {
-          title: "Community",
-          items: [
-            {
-              label: "Governance",
-              href: "https://gov.uniswap.org/",
-            },
-            {
-              label: "Discord",
-              href: "https://discord.gg/ybKVQUWb4s",
-            },
-            {
-              label: "Twitter",
-              href: "https://twitter.com/Uniswap",
-            },
-            {
-              label: "Blog",
-              href: "https://uniswap.org/blog/",
+              label: 'GitHub',
+              href: 'https://github.com/facebook/docusaurus',
             },
           ],
         },
       ],
-      // copyright: `unlicensed`,
+      copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
     },
-    colorMode: {
-      // "light" | "dark"
-      defaultMode: "dark",
-
-      // Hides the switch in the navbar
-      // Useful if you want to support a single color mode
-      disableSwitch: false,
-
-      // Should we use the prefers-color-scheme media-query,
-      // using user system preferences, instead of the hardcoded defaultMode
-      respectPrefersColorScheme: true,
-
-      // Dark/light switch icon options
-      switchConfig: {
-        // Icon for the switch while in dark mode
-        darkIcon: "\u{263D}",
-
-        // Unicode icons such as '\u2600' will work
-        // Unicode with 5 chars require brackets: '\u{1F602}'
-        lightIcon: "\u{263C}",
-      },
+    prism: {
+      theme: lightCodeTheme,
+      darkTheme: darkCodeTheme,
     },
   },
   presets: [
     [
-      "@docusaurus/preset-classic",
+      '@docusaurus/preset-classic',
       {
         docs: {
-          path: "protocol",
-          routeBasePath: "protocol/",
-          sidebarPath: require.resolve("./sidebars.js"),
-          editUrl: "https://github.com/uniswap/uniswap-docs/tree/main/",
-          includeCurrentVersion: false,
+          sidebarPath: require.resolve('./sidebars.js'),
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/',
         },
-
+        blog: {
+          showReadingTime: true,
+          // Please change this to your repo.
+          editUrl:
+            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+        },
         theme: {
-          customCss: require.resolve("./src/css/custom.css"),
-          customCss2: require.resolve("./src/css/colors.css"),
+          customCss: require.resolve('./src/css/custom.css'),
         },
-      },
-    ],
-  ],
-  plugins: [
-    [
-      "@docusaurus/plugin-content-docs",
-      {
-        id: "SDK",
-        path: "sdk",
-        routeBasePath: "sdk/",
-        sidebarPath: require.resolve("./sdkSidebars.js"),
-        includeCurrentVersion: false,
       },
     ],
   ],
